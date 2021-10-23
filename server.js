0ab3e391905e1271c69980f4932153fa9f32c916
Jessica Moody <jessicamoody1988@gmail.com>
2021-10-23 15:22:22 -0500

    Install dependencies & basic express setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// process.env.PORT looks for environment variable called port
+// When we deploy to Heroku that's where it's go9ing to get the port number
+// locally this will run on port 5000
+const PORT = process.env.PORT || 5000;
+
+// Listen on a port 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
