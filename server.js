51e5321ee8e80723ebb387a48b8301fc7e9deb87
Jessica Moody <jessicamoody1988@gmail.com>
2021-10-23 17:24:24 -0500

    Route Files with Express Router

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 // process.env.PORT looks for environment variable called port
 // When we deploy to Heroku that's where it's go9ing to get the port number
 // locally this will run on port 5000
